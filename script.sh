4e2cd078942b54bc7e21850a0332759effb1bda2
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2019-09-18 10:23:03 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
+#Edit these paths
 file1=INSERT FILE1 FILE PATH
 file2=INSERT FILE2 FILE PATH
+outfile=INSERT OUTFILE PATH
 
 #Extract individuals reporting living with their spouse
 grep -w 1 ${file1} | awk '{print $1}' > file3.txt
 
 Rscript Spouse.R \
 ${file2} \
-${file3}
+${file3} \
+${outfile}
