3ce60032cf73d180d9dfcd211f18817a3ad3642a
Guillaume Butler-Laporte <guillaume_bl@hotmail.com>
2022-12-17 23:47:20 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,10 @@
 #Edit these paths
-file1=INSERT FILE1 FILE PATH
-file2=INSERT FILE2 FILE PATH
-outfile=INSERT OUTFILE PATH
+file1=/path/to/file1.txt
+file2=/path/to/file2.txt
+pathOut=/path/to/output/folder/
+pathSpouseR=/path/to/Spouse.R
 
 #Extract individuals reporting living with their spouse
-grep -w 1 ${file1} | awk '{print $1}' > file3.txt
+grep -w 1 ${file1} | awk '{print $1}' > ${pathOut}file3.txt
 
-Rscript Spouse.R \
-${file2} \
-${file3} \
-${outfile}
+Rscript ${pathSpouseR} ${file2} ${pathOut}file3.txt ${pathOut}couples.txt
